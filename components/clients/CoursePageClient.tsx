835791e88a1848b17198f0d1c41341306ffcb49e
adilsaascraft <adilsaascraft@gmail.com>
2025-12-29 18:45:49 +0530

    vv

--- components/clients/CoursePageClient.tsx
+++ components/clients/CoursePageClient.tsx
@@ -145,7 +145,7 @@ export default function CoursePageClient({
           ))}
         </div>
       ) : filteredCourses.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {paginatedCourses.map((course) => (
             <CourseCard
               key={course._id}
